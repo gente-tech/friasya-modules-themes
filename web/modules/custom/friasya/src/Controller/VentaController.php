6706a6ed9fac6b02f0bb94412399e107cc7c0743
gente-tech <gente.tech.co@gmail.com>
2025-05-10 00:28:58 -0500

    Update VentaController.php

--- web/modules/custom/friasya/src/Controller/VentaController.php
+++ web/modules/custom/friasya/src/Controller/VentaController.php
@@ -12,7 +12,7 @@ class VentaController extends ControllerBase {
   public function build() {
     return [
       '#theme' => 'friasya_form_venta',
-      '#form' => \Drupal::formBuilder()->getForm('Drupal\friasya\Form\VentaForm'),
+      'form' => \Drupal::formBuilder()->getForm('Drupal\friasya\Form\VentaForm'),
       '#attached' => [
         'library' => ['friasya/form_venta'],
       ],
