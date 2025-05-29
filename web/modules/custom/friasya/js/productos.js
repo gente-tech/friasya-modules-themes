38ae3eadd2e44ae693dd4996fc768a5f4f031d5c
gente-tech <gente.tech.co@gmail.com>
2025-05-29 16:00:35 -0500

    Update productos.js

--- web/modules/custom/friasya/js/productos.js
+++ web/modules/custom/friasya/js/productos.js
@@ -8,6 +8,26 @@
       });
     }
   };
+  function actualizarTotales() {
+    let totalFacturacion = 0;
+    let totalGanancia = 0;
+    let totalReinversion = 0;
+
+    $('table.ventas-reporte tbody tr').each(function () {
+      const fact = $(this).find('.facturacion').text().replace(/[^0-9]/g, '') || '0';
+      const gan = $(this).find('.ganancia').text().replace(/[^0-9]/g, '') || '0';
+      const reinv = $(this).find('.reinversion').text().replace(/[^0-9]/g, '') || '0';
+
+      totalFacturacion += parseInt(fact);
+      totalGanancia += parseInt(gan);
+      totalReinversion += parseInt(reinv);
+    });
+
+    $('.total-facturacion').text('$' + totalFacturacion.toLocaleString());
+    $('.total-ganancia').text('$' + totalGanancia.toLocaleString());
+    $('.total-reinversion').text('$' + totalReinversion.toLocaleString());
+  }
+
   $(document).ready(function () {
     $('.costo-input').on('input', function () {
       const $row = $(this).closest('tr');
@@ -26,8 +46,16 @@
       } else {
         $row.find('.facturacion, .ganancia, .reinversion').text('$0');
       }
+
+      // Actualizar totales después de cada input
+      actualizarTotales();
     });
+
+    // Ejecutar una vez al cargar por si hay datos prellenados
+    actualizarTotales();
   });
 })(jQuery, Drupal);
+
+
 
 
