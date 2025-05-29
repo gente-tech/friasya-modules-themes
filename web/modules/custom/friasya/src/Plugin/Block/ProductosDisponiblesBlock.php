1254760d0de5b7008d26aa322c27c604351bbd67
gente-tech <gente.tech.co@gmail.com>
2025-05-29 18:54:21 -0500

    Update ProductosDisponiblesBlock.php

--- web/modules/custom/friasya/src/Plugin/Block/ProductosDisponiblesBlock.php
+++ web/modules/custom/friasya/src/Plugin/Block/ProductosDisponiblesBlock.php
@@ -43,7 +43,7 @@ class ProductosDisponiblesBlock extends BlockBase {
       '#productos' => $items,
       '#attached' => [
         'library' => [
-          'friasya/form_venta',
+          'friasya/carrito',
         ],
       ],
     ];
